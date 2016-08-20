c57fa185e1245b1f7913aa934f3d60baffd7c588
bluu <bluu@me.com>
2016-08-20 01:22:51 -0700

    Add filter todos and tests

--- app/tests/api/todo-api.test.jsx
+++ app/tests/api/todo-api.test.jsx
@@ -62,4 +62,54 @@ describe('Todo API', () => {
       expect(todos).toEqual([]);
     });
   });
+
+  describe('filterTodos', () => {
+    const todos = [
+        {
+          id: 1,
+          text: 'Some Text',
+          completed: true
+        },
+        {
+          id: 2,
+          text: 'Other Text',
+          completed: false
+        },
+        {
+          id: 3,
+          text: 'Some Todo',
+          completed: true
+        }
+    ];
+
+    it('should return all items if showCompleted is true', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should not return all items if showCompleted is false', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should return all items if search text is empty', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return matching items if search text is not empty', () => {
+      const filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+
+      expect(filteredTodos.length).toBe(2);
+    });
+  });
 });
