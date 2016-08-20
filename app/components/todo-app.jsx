ce5b3ee732db7976ef450f2554530603e2ae401a
bluu <bluu@me.com>
2016-08-20 12:02:48 -0700

    Add completed and created timestamps

--- app/components/todo-app.jsx
+++ app/components/todo-app.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const uuid = require('node-uuid');
+const moment = require('moment');
 
 const TodoList = require('todo-list');
 const TodoForm = require('todo-form');
@@ -24,7 +25,9 @@ const TodoApp = React.createClass({
         {
           id: uuid(),
           text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: null
         }
       ]
     });
@@ -39,6 +42,7 @@ const TodoApp = React.createClass({
     const updatedTodos = this.state.todos.map(todo => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : null;
       }
 
       return todo;
--- app/components/todo-item.jsx
+++ app/components/todo-item.jsx
@@ -1,15 +1,24 @@
 const React = require('react');
+const moment = require('moment');
 
 const TodoItem = React.createClass({
   render: function () {
-    const { id, text, completed } = this.props;
+    const { id, text, completed, createdAt, completedAt } = this.props;
+
+    const renderDate = () => {
+      const message = completed ? 'Completed ' : 'Created ';
+      const timestamp = completed ? completedAt : createdAt;
+
+      return `${message}${moment.unix(timestamp).format('MMM Do YYYY @ h:mm a')}`;
+    };
 
     return (
       <div onClick={() => {
           this.props.onToggle(id);
         }}>
         <input type="checkbox" checked={ completed }/>
-        { text }
+        <p>{ text }</p>
+        <p>{ renderDate() }</p>
       </div>
     );
   }
