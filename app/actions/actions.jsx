6fcaae19bf97aac8efa7c06e88c206a5759ea338
bluu <bluu@me.com>
2016-08-25 13:27:42 -0700

    Add user todos privacy

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -32,7 +32,8 @@ export const startAddTodo = (text) => {
       completedAt: null
     };
 
-    const todoRef = firebaseRef.child('todos').push(todo);
+    const uid = getState().auth.uid;
+    const todoRef = firebaseRef.child(`users/${uid}/todos`).push(todo);
 
     return todoRef.then(() => {
       dispatch(addTodo({
@@ -52,7 +53,8 @@ export const addTodos = (todos) => {
 
 export const startAddTodos = () => {
   return (dispatch, getState) => {
-    const todosRef = firebaseRef.child('todos');
+    const uid = getState().auth.uid;
+    const todosRef = firebaseRef.child(`users/${uid}/todos`);
 
     return todosRef.once('value').then(snapshot => {
       const todos = snapshot.val() || {};
@@ -80,7 +82,8 @@ export const updateTodo = (id, updates) => {
 
 export const startToggleTodo = (id, completed) => {
   return (dispatch, getState) => {
-    const todoRef = firebaseRef.child(`todos/${id}`);
+    const uid = getState().auth.uid;
+    const todoRef = firebaseRef.child(`users/${uid}/todos/${id}`);
 
     const updates = {
       completed,
